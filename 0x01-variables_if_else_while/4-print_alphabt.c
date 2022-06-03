49c0fef1b256939538faa7ca9b3474dda882d5db
Charles Duruaku <charlesrichson@gmail.com>
2022-06-03 14:47:52 +0100

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	for (letter < 'z'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
