e74e215b82f24e9919f75fc67d2348fc060f22fb
Charles Duruaku <charlesrichson@gmail.com>
2022-06-07 14:54:32 +0100

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - checks for aplha character
+ * @c: the character to check
+ * Return: 1 if c is alpha, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
