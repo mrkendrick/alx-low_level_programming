6e0925d297b210aef66912947fc5e377c237190e
Charles Duruaku <charlesrichson@gmail.com>
2022-06-16 10:53:57 +0100

    fixed issue

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,54 +1,51 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_buffer - prints buffer
- * @b: buffer
- * @size: size
- * Return: void
-*/
-
+ * print_buffer - Prints a buffer
+ * @b: char
+ * @size: int
+ * Return:void
+ */
 void print_buffer(char *b, int size)
 {
-	int o, j, i;
+	int x, i;
 
-	o = 0;
+	for (x = 0; x < size; x += 10)
+	{
+		printf("%08x: ", x);
 
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
-	while (o < size)
-	{
-		j = size - o < 10 ? size - o : 10;
-		printf("%08x: ", o);
 		for (i = 0; i < 10; i++)
 		{
-			if (i < j)
-			{
-				printf("%02x", *(b + o + i));
-			}
-			else
-			{
+			if ((i + x) >= size)
 				printf("  ");
-			}
-			if (i % 2)
-			{
-				printf(" ");
-			}
-		}
-		for (i = 0; i < j; i++)
-		{
-			int c = *(b + o + i);
 
-			if (c < 32 || c > 132)
-			{
-				c = '.';
-			}
-			printf("%c", c);
+			else
+				printf("%02x", *(b + i + x));
+
+			if ((i % 2) != 0 && i != 0)
+				printf(" ");
 		}
+
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + x) >= size)
+				break;
+
+			else if (*(b + i + x) >= 31 &&
+				 *(b + i + x) <= 126)
+				printf("%c", *(b + i + x));
+
+			else
+				printf(".");
+		}
+
+		if (x >= size)
+			continue;
+
 		printf("\n");
-		o += 10;
 	}
+
+	if (size <= 0)
+		printf("\n");
 }
