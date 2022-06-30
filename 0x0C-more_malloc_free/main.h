24a2f23785c770813034334640627ff3a6d78ef4
Charles Duruaku <charlesrichson@gmail.com>
2022-06-30 18:41:35 +0100

    more malloc, free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,4 +7,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _strlen(char *s);
+int is_digit(char *s);
 #endif
